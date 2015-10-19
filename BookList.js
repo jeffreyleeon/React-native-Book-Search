1de589d8ba2f4389d007d893887c7c6efbd6303f
JeffreyLee <jeffreyleeon@gmail.com>
2015-10-19 23:29:59 +0800

    Add dummy data

--- BookList.js
+++ BookList.js
@@ -3,13 +3,37 @@
 var React = require('react-native');
 
 var {
+  Image,
   StyleSheet,
+  Text,
   View,
   Component
 } = React;
 
 var styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    padding: 10
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+    marginRight: 10
+  },
+  rightContainer: {
+    flex: 1
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8
+  },
+  author: {
+    color: "#656565"
+  }
 });
 
 var FAKE_BOOK_DATA = [
@@ -18,10 +42,17 @@ var FAKE_BOOK_DATA = [
 
 class BookList extends Component {
   render() {
+    var book = FAKE_BOOK_DATA[0];
     return (
-      <View>
+      <View style={styles.container}>
+        <Image source={{uri: book.volumeInfo.imageLinks.thumbnail}}
+               style={styles.thumbnail} />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{book.volumeInfo.title}</Text>
+          <Text style={styles.author}>{book.volumeInfo.authors}</Text>
+        </View>
       </View>
-    )
+    );
   }
 }
 
