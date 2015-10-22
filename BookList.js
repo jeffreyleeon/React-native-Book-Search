9f90f5f192066d85ddcb978a70dd4158964550ca
JeffreyLee <jeffreyleeon@gmail.com>
2015-10-22 23:55:06 +0800

    Add a detail view for book

--- BookList.js
+++ BookList.js
@@ -1,13 +1,17 @@
 'use strict';
 
 var React = require('react-native');
+var BookDetail = require('./BookDetail');
 
 var {
   Image,
   StyleSheet,
   Text,
   View,
-  Component
+  Component,
+  ListView,
+  TouchableHighlight,
+  ActivityIndicatorIOS
 } = React;
 
 var styles = StyleSheet.create({
@@ -33,25 +37,104 @@ var styles = StyleSheet.create({
   },
   author: {
     color: "#656565"
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  listView: {
+    marginTop: 65,
+    marginBottom: 50,
+    backgroundColor: '#F5FCFF'
+  },
+  loading: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 });
 
-var FAKE_BOOK_DATA = [
-    {volumeInfo: {title: 'The Catcher in the Rye', authors: "J. D. Salinger", imageLinks: {thumbnail: 'http://books.google.com/books/content?id=PCDengEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'}}}
-];
+var REQUEST_URL = 'https://www.googleapis.com/books/v1/volumes?q=subject:fiction';
 
 class BookList extends Component {
-  render() {
-    var book = FAKE_BOOK_DATA[0];
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2
+      })
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch(REQUEST_URL)
+    .then(response => response.json())
+    .then(responseData => {
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(responseData.items),
+        isLoading: false
+      });
+    })
+    .done();
+  }
+
+  showBookDetail(book) {
+    this.props.navigator.push({
+      title: book.volumeInfo.title,
+      component: BookDetail,
+      passProps: {book}
+    });
+  }
+
+  renderBook(book) {
     return (
-      <View style={styles.container}>
-        <Image source={{uri: book.volumeInfo.imageLinks.thumbnail}}
-               style={styles.thumbnail} />
-        <View style={styles.rightContainer}>
-          <Text style={styles.title}>{book.volumeInfo.title}</Text>
-          <Text style={styles.author}>{book.volumeInfo.authors}</Text>
+      <TouchableHighlight
+        onPress={
+          () => this.showBookDetail(book)
+        }
+        underlayColor='#dddddd'>
+        <View>
+          <View style={styles.container}>
+            <Image
+              source={{uri: book.volumeInfo.imageLinks.thumbnail}}
+              style={styles.thumbnail} />
+            <View style={styles.rightContainer}>
+              <Text style={styles.title}>{book.volumeInfo.title}</Text>
+              <Text style={styles.authors}>{book.volumeInfo.authors}</Text>
+            </View>
+          </View>
+          <View style={styles.separator} />
         </View>
+      </TouchableHighlight>
+    );
+  }
+
+  renderLoadingView() {
+    return (
+      <View style={styles.loading}>
+        <ActivityIndicatorIOS
+          size='large'/>
+        <Text>
+          Loading books...
+        </Text>
       </View>
+    );
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return this.renderLoadingView();
+    };
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderBook.bind(this)}
+        style={styles.listView} />
     );
   }
 }
